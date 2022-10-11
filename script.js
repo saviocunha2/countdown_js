e4b02bcd01a8d150861d5d1da4af36fb54408614
Sávio Cunha <60552629+saviocunha2@users.noreply.github.com>
2022-10-11 16:45:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+import Countdown from "./countdown.js";
+
+const diasParaONatal = new Countdown('24 December 2022 23:59:59 GMT-0300');
+
+console.log(diasParaONatal.total);
+// console.log(diasParaONatal.hours);
